cd3d7d59ef39aa1c121a76217cd654a18ffb6807
Juan Grajales <grajales.juanfe11@gmail.com>
2020-03-22 07:21:07 -0400

    new react app for udemy course

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Day 1</h2>
+      <ul>
+        <li>How to be successful in class</li>
+      </ul>
     </div>
   );
 }
